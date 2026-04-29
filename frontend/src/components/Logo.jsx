2b8b8cc9523518d554404ece8c8fa31df0e3c574
Nourel <nourel@example.com>
2026-04-29 16:47:35 +0100

    full project clean

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,11 +1,19 @@
 // src/components/Logo.jsx
-import { LogoIcon } from './Icons';
-
 export default function Logo() {
   return (
-    <div className="logo">
-      <div className="logo__icon"><LogoIcon /></div>
-      <span className="logo__text">Stag.io</span>
+    <div className="logo-v2">
+      <div className="logo-v2__icon">
+        {/* Animated orbital rings */}
+        <div className="logo-v2__ring logo-v2__ring--1" />
+        <div className="logo-v2__ring logo-v2__ring--2" />
+        {/* Core dot */}
+        <div className="logo-v2__core">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="white"><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></svg>
+        </div>
+      </div>
+      <span className="logo-v2__text">
+        Stag<span className="logo-v2__dot">.io</span>
+      </span>
     </div>
   );
 }
